160418114a118c79444b7999d420e09ce8b24549
Awesomedusky <Awesomedursky@gmail.com>
2022-06-21 21:19:00 +0000

    new commit

--- I4G003480IAB/blog/models.py
+++ I4G003480IAB/blog/models.py
@@ -11,10 +11,10 @@ class current_user_model(models.Model):
 
 
 
-
 class Post(models.Model):
     Title = models.CharField(max_length=200)
     Text = models.TextField()
-    Author = models.ForeignKey(current_user_model,on_delete = models.CASCADE,)
+    Author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE)
     Created_date = models.DateTimeField(auto_now_add=True)
     Published_date = models.DateTimeField(auto_now_add=True)
